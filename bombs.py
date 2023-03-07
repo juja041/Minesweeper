5c05f8aba38d6ed5ec9e49ac878cbd07a02dac52
Ivo Walach <ivo.walach@gmail.com>
2023-03-07 19:45:52 +0100

    konec asi

--- bombs.py
+++ bombs.py
@@ -1,122 +1,161 @@
 import random
 
-list_squares=[i for i in range(100)]
-list_bombs = random.sample(list_squares, 20)
-for i in list_bombs:
-    list_squares[i] = True
+def bombicky():
+    global list_squares
+    list_squares=[i for i in range(100)]
+    global list_bombs
+    list_bombs = random.sample(list_squares, 20)
+    for i in list_bombs:
+        list_squares[i] = True
 
-print(list_squares)
-
-number_of_adjacent_bombs = []
-
-for i in list_squares:
+    print(list_squares)
     
-    if i is True:
-        number_of_adjacent_bombs.append(True)
-    
-    else:
+    global number_of_adjacent_bombs
+    number_of_adjacent_bombs = []
+
+
+            
+
+    for i in list_squares:
         
-        if i%10 == 0:
-            
-            if i == 0:
-                
-                numb=0
-                for a in [i+1,i+10,i+11]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)    
-                        
-            elif i==90:
-                
-                numb=0
-                for a in [i+1,i-10,i-9]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)
-            
-            else:
-                
-                numb=0
-                for a in [i+1,i-10,i+10,i-9,i+11]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)
+        if i is True:
+            number_of_adjacent_bombs.append(True)
         
-        elif i%10 == 9:
-            
-            if i == 9:
-                 
-                numb=0
-                for a in [i-1,i+10,i+9]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)
-            
-            elif i == 99:
-                
-                numb=0
-                for a in [i-1,i-10,i-11]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)
-            
-            else:
-                
-                numb=0
-                for a in [i-1,i-10,i-11,i+9,i+10]:
-                    
-                    if list_squares[a] is True:
-                        numb+=1
-                
-                number_of_adjacent_bombs.append(numb)
-        
-        elif i<9:
-            
-            numb=0
-            for a in [i-1,i+1,i+10,i+9,i+11]:
-                    
-                if list_squares[a] is True:
-                    numb+=1
-                
-            number_of_adjacent_bombs.append(numb)
-            
-        elif i > 90:
-            
-            numb=0
-            for a in [i-1,i+1,i-10,i-9,i-11]:
-                    
-                if list_squares[a] is True:
-                    numb+=1
-                
-            number_of_adjacent_bombs.append(numb)
-            
         else:
             
-            numb=0
-            for a in [i-1,i+1,i-10,i-9,i-11,i+9,i+10,i+11]:
-                    
-                if list_squares[a] is True:
-                    numb+=1
+            if i%10 == 0:
                 
-            number_of_adjacent_bombs.append(numb)
+                if i == 0:
+                    
+                    numb=0
+                    for a in [i+1,i+10,i+11]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)    
+                            
+                elif i==90:
+                    
+                    numb=0
+                    for a in [i+1,i-10,i-9]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)
+                
+                else:
+                    
+                    numb=0
+                    for a in [i+1,i-10,i+10,i-9,i+11]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)
             
+            elif i%10 == 9:
+                
+                if i == 9:
+                    
+                    numb=0
+                    for a in [i-1,i+10,i+9]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)
+                
+                elif i == 99:
+                    
+                    numb=0
+                    for a in [i-1,i-10,i-11]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)
+                
+                else:
+                    
+                    numb=0
+                    for a in [i-1,i-10,i-11,i+9,i+10]:
+                        
+                        if list_squares[a] is True:
+                            numb+=1
+                    
+                    number_of_adjacent_bombs.append(numb)
+            
+            elif i<9:
+                
+                numb=0
+                for a in [i-1,i+1,i+10,i+9,i+11]:
+                        
+                    if list_squares[a] is True:
+                        numb+=1
+                    
+                number_of_adjacent_bombs.append(numb)
+                
+            elif i > 90:
+                
+                numb=0
+                for a in [i-1,i+1,i-10,i-9,i-11]:
+                        
+                    if list_squares[a] is True:
+                        numb+=1
+                    
+                number_of_adjacent_bombs.append(numb)
+                
+            else:
+                
+                numb=0
+                for a in [i-1,i+1,i-10,i-9,i-11,i+9,i+10,i+11]:
+                        
+                    if list_squares[a] is True:
+                        numb+=1
+                    
+                number_of_adjacent_bombs.append(numb)
+                
+    global list_positions
+    list_positions = []            
+    for i in range(10):
+        for u in range(10):
+            list_positions.append((i*50,u*50))            
+    global dict_adj_pos                    
+    dict_adj_pos = dict(zip(list_positions,number_of_adjacent_bombs))    
+    print(number_of_adjacent_bombs)
+    print(list_positions)
+    print(dict_adj_pos)
 
-list_positions = []            
-for i in range(10):
-    for u in range(10):
-        list_positions.append((i*50,u*50))            
-                     
-dict_adj_pos = dict(zip(list_positions,number_of_adjacent_bombs))    
-print(number_of_adjacent_bombs)
-print(list_positions)
-print(dict_adj_pos)
+def adjacent(i):
+    num = list_positions.index(i)
+    if num%10 == 0:
+        if num == 0:
+            return (list_positions[num+1],list_positions[num+10],list_positions[num+11])
+        
+        elif num == 90:
+            return (list_positions[num+1],list_positions[num-10],list_positions[num-9])
+        
+        else:
+            return (list_positions[num+1],list_positions[num-10],list_positions[num+10],list_positions[num+11], list_positions[num-9])
+            
+    elif num%10 == 9:
+        
+        if num == 9:
+            return (list_positions[num-1],list_positions[num+10],list_positions[num+9])
+        
+        elif num == 99:
+            return (list_positions[num-1],list_positions[num-10],list_positions[num-11])
+        
+        else:
+            return (list_positions[num-1],list_positions[num-10],list_positions[num-11],list_positions[num+9],list_positions[num+10])
+    
+    elif num<9:
+        return (list_positions[num-1],list_positions[num+1],list_positions[num+10],list_positions[num+9],list_positions[num+11])
+    
+    elif num > 90:
+        return (list_positions[num-1],list_positions[num+1],list_positions[num-10],list_positions[num-9],list_positions[num-11])
+    
+    else:
+        return (list_positions[num-1],list_positions[num+1],list_positions[num-10],list_positions[num-9],list_positions[num-11],list_positions[num+9],list_positions[num+10],list_positions[num+11])
--- main.py
+++ main.py
@@ -2,35 +2,54 @@ import pygame
 import os
 import random
 import bombs
+from bombs import bombicky
 import time
 pygame.font.init()
+
 
 WIDTH, HEIGHT = 500,500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 FPS = 15
 pygame.display.set_caption("Minesweeper")
 
-UNCOVERED_SQUARE_IMG = pygame.image.load(r'C:\learn\local_stuff\miny\Minesweeper\Images\uncovered_square.png')
+UNCOVERED_SQUARE_IMG = pygame.image.load(r'Minesweeper\Images\uncovered_square.png')
 UNCOVERED_SQUARE = pygame.transform.scale(UNCOVERED_SQUARE_IMG, (50,50))
 #MINE_IMAGE = pygame.image.load(r'C:\learn\local_stuff\miny\Minesweeper\Images\mina.png')
 #MINE = pygame.transform.scale(MINE_IMAGE, (50,50))
 
-Font = pygame.font.SysFont("comicsans", 40)
-gameover = Font.render("ENTER TO PLAY AGAIN", 1, (255,182,193))
+Font = pygame.font.SysFont("comicsans", 30)
+gameover = Font.render("YOU LOST, ENTER TO PLAY AGAIN", 1, (0,0,0))
+win = Font.render("YOU WON, ENTER TO PLAY AGAIN", 1, (0,0,0))
+
 
 images = []
 for i in range(9):
-    images.append(pygame.transform.scale(pygame.image.load(f'C:\learn\local_stuff\miny\Minesweeper\Images\{i}.png'), (50,50)))
+    images.append(pygame.transform.scale(pygame.image.load(f'Minesweeper\Images\{i}.png'), (50,50)))
 
-images.append(pygame.transform.scale(pygame.image.load(r'C:\learn\local_stuff\miny\Minesweeper\Images\bomb-at-clicked-block.png'), (50,50)))
-images.append(pygame.transform.scale(pygame.image.load(r'C:\learn\local_stuff\miny\Minesweeper\Images\unclicked-bomb.png'), (50,50)))
+images.append(pygame.transform.scale(pygame.image.load(r'Minesweeper\Images\bomb-at-clicked-block.png'), (50,50)))
+images.append(pygame.transform.scale(pygame.image.load(r'Minesweeper\Images\unclicked-bomb.png'), (50,50)))
 
 positions_already_clicked=set()
 
 
-ENDOFGAME = False
+""" def zero_helper(position):
+    listt = []
+    listek=[]
+    neco = bombs.adjacent(position)
+    for i in neco:
+        if bombs.dict_adj_pos.get(i) == 0:
+            WIN.blit(images[bombs.dict_adj_pos.get(i)], i)
+            zero_helper(i)
+        else:
+            WIN.blit(images[bombs.dict_adj_pos.get(i)], i) """
+            
+    
+
 
 coordinates_list = []
+blited_stuff = set()
+ez = True
+
 def draw_window():
     WIN.fill((255,255,255))
     
@@ -46,23 +65,114 @@ def draw_window():
             for y in bombs.dict_adj_pos:
                 if bombs.dict_adj_pos.get(y) is True:
                     WIN.blit(images[10],y)
+                    blited_stuff.add(y)
                 else:
                     WIN.blit(images[bombs.dict_adj_pos.get(y)], y)
+                    blited_stuff.add(y)
             WIN.blit(images[9], i)
-            global ENDOFGAME
-            ENDOFGAME = True
-            #positions_already_clicked = set()
+            blited_stuff.add(i)
+            WIN.blit(gameover, (0,200))
             
             
-            #WIN.blit(gameover, (250-gameover.get_width()//2,250-gameover.get_height()//2))
+            global ez
+            ez = False
+            global GGEZ
+            GGEZ = True
+            global winn
+            winn = False
+            
+        
+        elif what == 0:
+            WIN.blit(images[0], i)
+            blited_stuff.add(i)
+            #zero_helper(i)
+            listt=[]
+            listek=[]
+            blbost = [i]
+            
+            for a in bombs.adjacent(i):
+                WIN.blit(images[bombs.dict_adj_pos.get(a)], a)
+                blited_stuff.add(a)
+                if bombs.dict_adj_pos.get(a) == 0:
+                    for w in bombs.adjacent(a):
+                        WIN.blit(images[bombs.dict_adj_pos.get(w)], w)
+                        blited_stuff.add(w)
+                        if bombs.dict_adj_pos.get(w) == 0:
+                            for z in bombs.adjacent(w):
+                                WIN.blit(images[bombs.dict_adj_pos.get(z)], z)
+                                blited_stuff.add(z)
+                                if bombs.dict_adj_pos.get(z) == 0:
+                                    for u in bombs.adjacent(z):
+                                        WIN.blit(images[bombs.dict_adj_pos.get(u)], u)
+                                        blited_stuff.add(u)
+                                        '''if bombs.dict_adj_pos.get(u) == 0:
+                                            for l in bombs.adjacent(u):
+                                                WIN.blit(images[bombs.dict_adj_pos.get(l)], l)
+                                                blited_stuff.add(l)'''
+                                    
+            
+            """ while True:
+                for h in blbost:     
+                    for a in bombs.adjacent((i[0]+(i[0]-h[0]),i[1]+(i[1]-h[1]))):
+                        WIN.blit(images[bombs.dict_adj_pos.get(a)], a)
+                        if bombs.dict_adj_pos.get(a) == 0:
+                            listt.append(True)
+                            listek.append(a)
+                        else:
+                            listt.append(False)
+                    
+                    if not any(listt):
+                        break
+                    blbost.clear()
+                    for w in listek:
+                        blbost.append(w)
+                    listek.clear() """
+
+                
+                    
+                    
+                    
+            
             
         else:
             WIN.blit(images[what], i)
+            blited_stuff.add(i)
+    
+    coord_set = set(coordinates_list)
+    diference = coord_set - blited_stuff
+    dif = set()
+    for i in diference:
+        dif.add(bombs.dict_adj_pos[i])
+    if len(dif) == 1 and list(dif)[0] is True:
+        for y in bombs.dict_adj_pos:
+            if bombs.dict_adj_pos.get(y) is True:
+                WIN.blit(images[10],y)
+            else:
+                WIN.blit(images[bombs.dict_adj_pos.get(y)], y)
+        
+        GGEZ = True
+        
+
+        
+    
+        
+        
     
     
     pygame.display.update()
 
 def main():
+    global positions_already_clicked
+    positions_already_clicked=set()
+    global blited_stuff
+    blited_stuff = set()
+    global coordinates_list
+    coordinates_list = []
+    global GGEZ
+    GGEZ = False
+    global winn
+    winn=False
+    bombicky()
     #positions_already_clicked = set()
     clock = pygame.time.Clock()
     run = True
@@ -82,6 +192,15 @@ def main():
         
         
         draw_window()
+        
+        if GGEZ == True:    
+            while True:
+                
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key== pygame.K_RETURN:
+                            main()
+        
         '''if ENDOFGAME == True:
             while True:
                 for event in pygame.event.get():
@@ -103,4 +222,29 @@ def main():
 if __name__ == "__main__":
     main()
         
-        
+
+
+""" listt=[]
+        listek=[]
+        blbost = [i]
+        while True:
+            for h in blbost:     
+                for a in bombs.adjacent((i[0]+(i[0]-h[0]),i[1]+(i[1]-h[1]))):
+                    WIN.blit(images[bombs.dict_adj_pos.get(a)], a)
+                    if bombs.dict_adj_pos.get(a) == 0:
+                        listt.append(True)
+                        listek.append(a)
+                    else:
+                        listt.append(False)
+                
+                if not any(listt):
+                    break
+                blbost.clear()
+                for w in listek:
+                    blbost.append(w)
+                listek.clear() """
+                    
+                    
+                
+                #for h in listek:
+                    #for w in bombs.adjacent(h)
